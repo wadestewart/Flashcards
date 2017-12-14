3dbbcb0adcc49de7ec9bee5393ab1bd599636da1
Wade Stewart <stew.wade@gmail.com>
2017-12-14 10:22:52 -0500

    added next card button, ending game on 5 correct

--- script.js
+++ script.js
@@ -12,7 +12,9 @@
 // Bonus: user adds thier own cards
 
 
+
 // Array containing data for flash cards
+let counter = 0
 const gameCards = [
     {
         Question: "Alexandra 'Alex' Owens",
@@ -21,7 +23,7 @@ const gameCards = [
 
     {
         Question: 'Nick Hurley',
-        Answer: 'Owner of the steel mill where ALex works, he falls for her after seeing her dance exotically. *sidenote: he would later resign for the obvious sexual harrasment.'
+        Answer: 'Owner of the steel mill where Alex works, he falls for her after seeing her dance exotically. *sidenote: he would later resign for the obvious sexual harrasment.'
     },
 
     {
@@ -40,65 +42,72 @@ const gameCards = [
     }
 ]
 
-// const eachCard = []
-// add counter
-// you want this loop be driven user guess rather than a for loop
-// for (let i = 0; i > gameCards.length; i++) {
-//     let 
-// }
-
-// let counter = 0
-// Game opening screen, click Begin to start study session!
-// Begin button, front/back of card targeted
-let begin = $('#begin')
-let cardFront = $('.front')
-let cardBack = $('.back')
+// Game opening screen, click Begin to start study session! Begin button, flip button, card, front/back of card targeted
+let card = $( '#card' )
+let begin = $( '#begin' )
+let flip = $( '#flip' )
+let cardFront = $( '.front' )
+let cardBack = $( '.back' )
 
 begin.on('click', firstCardQ)
 
+flip.click(function () {
+    card.toggleClass( 'flipped' )
+})
+
 // Function to populate 1st card question, change background, change 'Begin' button to 'Flip', and flip card when ready
 function firstCardQ() {
-    let cardPop1Q = gameCards[0].Question
-    cardFront.text(cardPop1Q).css({'backgroundImage': 'url(images/alex.jpg)'})
-    begin.html('Flip')
-    begin.click(function() {
-        $('#card').toggleClass('flipped')
-    })
+    let cardPop1Q = gameCards[0].Question// Add counter here
+    cardFront.text( cardPop1Q ).css({ 'backgroundImage': 'url(images/alex.jpg)' }).css( 'color', 'white' )
+    begin.off( 'click', firstCardQ)
 }
+
 // Function to populate 1st card answer
-begin.on('click', firstCardA)
+flip.on( 'click', firstCardA )
 
 function firstCardA() {
     let cardPop1A = gameCards[0].Answer
-    cardBack.text(cardPop1A)
+    cardBack.text( cardPop1A ).css( 'background', '#F090C0')
+    // flip.off( 'click', firstCardA)
 }
 
+// Next card event listener and function
 let next = $('#next')
 
-next.on('click', nextCard)
+next.on('click', secondCardQ)
 
-function nextCard () {
-    
+function secondCardQ () {
+    let cardPop2Q = gameCards[1].Question
+    cardFront.text( cardPop2Q ).css({'backgroundImage': 'url(images/nick.jpg)'}).css( 'text-align', 'left' )
 }
 
-let correct = $('#buttonC')
-let tallyCorrect = parseInt($('.correct').text())
+flip.on( 'click', secondCardA)
+
+function secondCardA () {
+    let cardPop2A = gameCards[1].Answer
+    cardBack.text( cardPop2A ).css( 'background', '#F090C0')
+}
+
+let correct = $( '#buttonC' )
+let tallyCorrect = parseInt($( '.correct' ).text())
 
 correct.on('click', () => {
     tallyCorrect += 1
 
-    if (tallyCorrect <= 5) {
-        $('.correct').text(tallyCorrect)
+    if ( tallyCorrect < 5 ) {
+        $( '.correct' ).text( tallyCorrect )
+    } else if ( tallyCorrect === 5 ) {
+        alert( 'You are a maniac!' )
     }
 })
 
-let incorrect = $('#buttonI')
-let tallyIncorrect = parseInt($('.incorrect').text())
+let incorrect = $( '#buttonI' )
+let tallyIncorrect = parseInt($( '.incorrect' ).text())
 
 incorrect.on('click', () => {
     tallyIncorrect += 1
 
-    if (tallyIncorrect <= 5) {
-        $('.incorrect').text(tallyIncorrect)
+    if ( tallyIncorrect <= 5 ) {
+        $( '.incorrect' ).text( tallyIncorrect )
     }
 })
