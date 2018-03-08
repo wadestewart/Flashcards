2d675ee3e64881ae14fc79b207bce3ae9e1fb346
Wade Stewart <stew.wade@gmail.com>
2018-03-08 13:54:11 -0500

    Iterating over array to render images

--- script.js
+++ script.js
@@ -4,42 +4,39 @@ let counter = 0
 const gameCards = [
     {
         Question: "Alexandra 'Alex' Owens",
-        Answer: 'A steel town welder with a dream of becoming a professional dancer. *sidenote: she works as an exotic dancer.'
+        Answer: 'A steel town welder with a dream of becoming a professional dancer.',
+        imgUrl: 'url(images/alex.jpg)'
     },
-
     {
         Question: 'Nick Hurley',
-        Answer: 'Owner of the steel mill where Alex works, he falls for her after seeing her dance exotically. *sidenote: he would later resign for the obvious sexual harassment.',
-        // imgFile: 'url(images.nick.jpg)'
+        Answer: 'Owner of the steel mill where Alex works, he falls for her after seeing her dance exotically. He would later resign after being swept up in the #MeToo and #TimesUp movement for his obvious sexual harassment.',
+        imgUrl: 'url(images/nick.jpg)'
     },
-
     {
         Question: "Mawby's",
         Answer: "'Gentleman' Club where Alex and her friends work. Many have aspirations beyond dancing exotically.",
-        // imgFile: 'url(images/mawbys.jpg)'
+        imgUrl: 'url(images/mawbys.jpg)'
     },
-
     {
         Question: 'Pittsburgh Conservatory of Dance and Repertory',
         Answer: 'Where Alex eventually gets an audition and her dreams come true!',
-        // imgFile:'images/pcdr.png'
+        imgUrl: 'url(images/pcdr.png)'
     },
-
     {
         Question: 'Maniac',
         Answer: "Title track from the movie. 'Best song ever written' -Mozart",
-        // imgFile: 'images/maniac.png'
+        imgUrl: 'url(images/maniac.png)'
     }
 ]
 
 // Game opening screen, click Begin to start study session! Begin button, flip button, card, front/back of card targeted
-let card = $( '#card' )
-let begin = $( '#begin' )
+let card = $('#card')
+let begin = $('#begin')
 let next = $('#next')
-let correct = $( '#buttonC' )
-let incorrect = $( '#buttonI' )
-let cardFront = $( '.front' )
-let cardBack = $( '.back' )
+let correct = $('#buttonC')
+let incorrect = $('#buttonI')
+let cardFront = $('.front')
+let cardBack = $('.back')
 let populateQ
 let populateA
 
@@ -50,24 +47,27 @@ begin.on('click', function() {
 // Function to populate card questions, change background, change 'Begin' button to 'Flip', and flip card when ready
 function cardQuestion (counter) {
     let populateQ = gameCards[counter].Question
-    cardFront.text( populateQ ).css({ 'backgroundImage': 'url(images/alex.jpg)' }).css( 'color', 'white' )
+    console.log(populateQ)
+    let populateImg = gameCards[counter].imgUrl
+    console.log(populateImg)
+    cardFront.text(populateQ).css({ 'backgroundImage': populateImg }).css('color', 'white')
     begin.text('Flip')
     begin.click(function () {
-        card.toggleClass( 'flipped' )
+        card.toggleClass('flipped')
     })
-    // begin.off()
+    begin.off()
 }
 
 // Function to populate 1st card answer
-begin.on( 'click', function() {
+begin.on('click', function() {
     cardAnswer(counter)
 })
 
-function cardAnswer (counter) {
+function cardAnswer(counter) {
     let populateA = gameCards[counter].Answer
-    cardBack.text( populateA ).css( 'background', '#F090C0')
+    cardBack.text( populateA )
     begin.click(function () {
-        card.toggleClass( 'flipped' )
+        card.toggleClass('flipped')
     })
 }
 
@@ -78,14 +78,14 @@ next.on('click', function() {
         counter = 0
     }
     cardQuestion(counter)
-    cardFront.text( populateQ ).css({ 'backgroundImage': 'url(images/nick.jpg)'}).css( 'text-align', 'left' )
-    cardFront.text( populateQ )//.css({ 'backgroundImage': 'url(images/mawbys.jpg'})
-    cardFront.text( populateQ )
-    cardFront.text( populateQ )
+    // cardFront.text(populateQ)
+    // cardFront.text(populateQ)
+    // cardFront.text(populateQ)
+    // cardFront.text(populateQ)
 })
 
 // Tally correct cards
-let tallyCorrect = parseInt($( '.correct' ).text())
+let tallyCorrect = parseInt($('.correct').text())
 
 correct.on('click', () => {
     tallyCorrect += 1
@@ -98,7 +98,7 @@ correct.on('click', () => {
 })
 
 // Tally incorrect cards
-let tallyIncorrect = parseInt($( '.incorrect' ).text())
+let tallyIncorrect = parseInt($('.incorrect').text())
 
 incorrect.on('click', () => {
     tallyIncorrect += 1
